89da3603c531118f769ccf2589bdccf4062de631
Vlasceanu, Fabian Florin <fabian.vlasceanu@miele.com>
2020-07-17 16:44:16 +0300

    Add LoginTests

---
+++ ActivityAppUITests/Tests/LoginTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Xamarin.UITest;
+using NUnit.Framework;
+using ActivityAppUITests.Pages;
+
+namespace ActivityAppUITests.Tests
+{
+    public class LoginTests: BaseTextFixture
+    {
+        public LoginTests(Platform platform): base(platform)
+        {
+
+        }
+        [SetUp]
+        public void NavigateToLoginBeforeEachTest() =>
+            new NavigationPageObject().NavigateToLoginPage();
+
+        [Test]
+        public void WaitForAllElementsLoginPage()
+        {
+            // Arrange 
+            var loginPageObject = new LoginPageObject();
+
+            // Act & Assert
+            loginPageObject.WaitForAllElements();
+
+        }
+
+    }
+}
